931b7a6ff38a0a46d57262efcdbe11817a0f1abd
Barış Pişmişoğlu <barispo@gmail.com>
2021-03-06 18:06:35 +0300

    Add project files.

---
+++ ClassMetotDemo/Musteri.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class Musteri
+    {
+        public int Id { get; set; }
+        public string Adi { get; set; }
+        public string Soyadi { get; set; }
+        public string kimlikNo { get; set; }
+        public string Sehir { get; set; }
+        public int cepTel { get; set; }
+
+    }
+}
---
+++ ClassMetotDemo/MusteriManager.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriManager
+    {
+
+
+
+        List<Musteri> musteriler = new List<Musteri>();
+
+
+        public void Ekle(Musteri musteri)
+        {
+            musteriler.Add(musteri);
+            Console.WriteLine("Müşteri Eklenmiştir  : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+        
+        public void Sil(Musteri musteri)
+        {
+            musteriler.Remove(musteri);
+            Console.WriteLine("Müşteri Silinnmiştir  : " + musteri.Adi + " " + musteri.Soyadi);
+        }
+
+        public void Liste(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Listesi : " + musteri.Adi + " " + musteri.Soyadi);
+            foreach (Musteri Musteri in musteriler)
+
+            {
+                Console.WriteLine("Müşteri Id : " + musteri.Id);
+                Console.WriteLine("Müşteri Adı : " + musteri.Adi);
+                Console.WriteLine("Müşteri Soyadı : " + musteri.Soyadi);
+                Console.WriteLine("Müşteri Kimlik No : " + musteri.kimlikNo);
+                Console.WriteLine("Müşteri Yaşadığı Şehir : " + musteri.Sehir);
+                Console.WriteLine("Müşteri Cep Tel.No. : " + musteri.cepTel);
+            }
+        }
+
+
+    }
+}
